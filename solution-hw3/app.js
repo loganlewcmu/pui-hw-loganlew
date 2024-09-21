e2bfc3ef282269be92310fdb9432e19f2432a1ba
loganlewcmu <loganlew@andrew.cmu.edu>
2024-09-21 18:18:54 -0400

    Cleaned code

--- solution-hw3/app.js
+++ solution-hw3/app.js
@@ -1,76 +1,77 @@
+//Create objects for glazing + pack size options//
 const glazingOptions =
-[
+  [
     {
-        glazingSelection: 'Keep original',
-        priceAdaptation: '$0.00',
-      },
-      {
-        glazingSelection: 'Sugar milk',
-        priceAdaptation: '$0.00',
-      },
-      {
-        glazingSelection: 'Vanilla milk',
-        priceAdaptation: '$0.50',
-      },
-      {
-        glazingSelection: 'Double chocolate',
-        priceAdaptation: '$1.50',
-      },
-]
+      glazingSelection: 'Keep original',
+      priceAdaptation: '$0.00',
+    },
+    {
+      glazingSelection: 'Sugar milk',
+      priceAdaptation: '$0.00',
+    },
+    {
+      glazingSelection: 'Vanilla milk',
+      priceAdaptation: '$0.50',
+    },
+    {
+      glazingSelection: 'Double chocolate',
+      priceAdaptation: '$1.50',
+    },
+  ]
 
 const packSizeOptions =
-[
+  [
     {
-        packSizeSelection: '1',
-        priceAdaptation: 1,
-      },
-      {
-        packSizeSelection: '3',
-        priceAdaptation: 3,
-      },
-      {
-        packSizeSelection: 6,
-        priceAdaptation: 5,
-      },
-      {
-        packSizeSelection: 12,
-        priceAdaptation: 10,
-      },
-    ]
+      packSizeSelection: '1',
+      priceAdaptation: 1,
+    },
+    {
+      packSizeSelection: '3',
+      priceAdaptation: 3,
+    },
+    {
+      packSizeSelection: 6,
+      priceAdaptation: 5,
+    },
+    {
+      packSizeSelection: 12,
+      priceAdaptation: 10,
+    },
+  ]
 
 //Dynamically populate glazing dropdown with 4 options//
 function loadGlazingOptions() {
-    const glazingDropdown = document.getElementById('pd_dropdown_glazing');  
+  const glazingDropdown = document.getElementById('pd_dropdown_glazing');
 
-    for (let i = 0; i < glazingOptions.length; i++) {
-        console.log(glazingOptions[i].glazingSelection);
-        const option = document.createElement('option');
-        option.text = glazingOptions[i].glazingSelection;  
-        option.value = i;
-        glazingDropdown.appendChild(option)
+  for (let i = 0; i < glazingOptions.length; i++) {
+    console.log(glazingOptions[i].glazingSelection);
+    const option = document.createElement('option');
+    option.text = glazingOptions[i].glazingSelection;
+    option.value = i;
+    glazingDropdown.appendChild(option)
 
-    }
+  }
 }
 
 loadGlazingOptions();
 
 //Dynamically populate pack size dropdown with 4 options//
 function loadPackSizeOptions() {
-  const packSizeDropdown = document.getElementById('pd_dropdown_packSize');  
+  const packSizeDropdown = document.getElementById('pd_dropdown_packSize');
 
   for (let i = 0; i < packSizeOptions.length; i++) {
-      console.log(packSizeOptions[i].packSizeSelection);
-      const option = document.createElement('option');
-      option.text = packSizeOptions[i].packSizeSelection;  
-      option.value = i;
-      packSizeDropdown.appendChild(option)
+    console.log(packSizeOptions[i].packSizeSelection);
+    const option = document.createElement('option');
+    option.text = packSizeOptions[i].packSizeSelection;
+    option.value = i;
+    packSizeDropdown.appendChild(option)
 
   }
 }
 
 loadPackSizeOptions();
 
-//update price
+//update price (on window load and in response to user inputs)
 
 const defaultPrice = 2.49
 let currentGlazingPrice = 0.00;

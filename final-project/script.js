99ca18bdd72cc7e0890ca8f499bfd6515183d8be
loganlewcmu <loganlew@andrew.cmu.edu>
2024-12-10 04:58:25 -0500

    Code complete
    Updated functionality, formatting, organized code

--- final-project/script.js
+++ final-project/script.js
@@ -67,26 +67,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-/* ----- Sidebar responsive behavior ----- */
-function toggleSidebar() {
-    const menuIcon = document.getElementById('menu_icon');
-    const sidebar = document.querySelector('.nav_sidebar');
-
-    // Toggle sidebar visibility and change the icon
-    if (sidebar.style.display === 'flex') {
-        sidebar.style.display = 'none';
-        menuIcon.innerHTML = '<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>';
-    } else {
-        sidebar.style.display = 'flex';
-        menuIcon.innerHTML = '<path d="M120-240v-80h520v80H120Zm664-40L584-480l200-200 56 56-144 144 144 144-56 56ZM120-440v-80h400v80H120Zm0-200v-80h520v80H120Z"/>';
-    }
-}
-
-// Hide sidebar on close
-function hideSidebar(){
-    const sidebar = document.querySelector('.nav_sidebar');
-    sidebar.style.display = 'none';
-}
 
 
 /* ----- Hover behavior for point of interest list items ----- */
